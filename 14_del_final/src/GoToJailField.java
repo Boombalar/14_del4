da60bd1a5d6afa85ff3931b2febba9786e46078c
KimSand <s@ndberg.dk>
2018-01-05 15:37:38 +0100

    GoToJail field færdig

---
+++ 14_del_final/src/GoToJailField.java
@@ -0,0 +1,9 @@
+
+public class GoToJailField extends Field{
+	int jail;
+	
+	public GoToJailField(String name, int type, int number, int jail) {
+		super(name, type, number);
+		this.jail = jail;
+	}
+}
