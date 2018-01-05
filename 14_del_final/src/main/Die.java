f1d29fa455e6ddf6418d46fbbcb8d4380666c69a
Dino1106 <niklaes@gmail.com>
2018-01-06 00:23:46 +0100

    Die finish, DieTest add

--- 14_del_final/src/main/Die.java
+++ 14_del_final/src/main/Die.java
@@ -1,21 +1,21 @@
+package main;
 import java.util.Random;
 
 public class Die {
 
 	private int eyes;
 	private int sides = 6;
-	
-	Die (int sides) {
-		this.sides = sides;
+
+	public Die () {
 	}
-	
+
 	public void roll() {
 		Random number = new Random();
 		this.eyes = number.nextInt(this.sides)+1;
 	}
-	
+
 	public int getEyes() { 
 		return this.eyes;
 	}
-	
+
 }
