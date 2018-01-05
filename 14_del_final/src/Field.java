7ab5076959fb6f4819b81f9b5df69035f15ad37b
KimSand <s@ndberg.dk>
2018-01-05 15:10:44 +0100

    Field klasse færdig.

--- 14_del_final/src/Field.java
+++ 14_del_final/src/Field.java
@@ -1,4 +1,24 @@
 
-public class Field {
+public abstract class Field {
+	protected String name;
+	protected int type;
+	protected int number;
 
+	public Field(String name, int type, int number) {
+		this.name = name;
+		this.type = type;
+		this.number = number;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getType() {
+		return this.type;
+	}
+	
+	public int getNumber() {
+		return this.number;
+	}
 }
