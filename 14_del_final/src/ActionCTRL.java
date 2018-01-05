74ff445e844403f23530f6a35323cedd0e028e35
KimSand <s@ndberg.dk>
2018-01-05 16:18:35 +0100

    ActionCTRL skabelon færdig

---
+++ 14_del_final/src/ActionCTRL.java
@@ -0,0 +1,22 @@
+
+public class ActionCTRL {
+	Player[] players;
+	Board board;
+	ViewCTRL view;
+	ChanceCardCTRL chancecard;
+	Diecup diecup;
+	
+	public ActionCTRL() {
+		
+	}
+	
+	private void initialiseGame() {
+		
+	}
+	
+	private void switchRules() {
+		
+	}
+	
+	//REGEL METODER HERUNDER.
+}
