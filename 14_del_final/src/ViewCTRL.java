34d8b4b5cacb97e3b04c163128b79b1a8e3909cf
KimSand <s@ndberg.dk>
2018-01-05 16:10:41 +0100

    ViewCTRL ramme færdig

---
+++ 14_del_final/src/ViewCTRL.java
@@ -0,0 +1,55 @@
+
+public class ViewCTRL {
+
+	
+	
+	public ViewCTRL(Player[] players, Board board) {
+		
+	}
+	
+	public String getDropDownChoice(String buttontext, String[] lines) {
+		String choice = "";
+		
+		return choice;
+	}
+	
+	public int getButtonPressed(String buttontext) {
+		int choice;
+		
+		return choice;
+	}
+	
+	public String getTextField() {
+		String enteredtext="";
+		
+		return enteredtext;
+	}
+	
+	public void updateDice(int die1, int die2) {
+		
+	}
+	
+	public void updatePlayerPosition(int player, int position) {
+		
+	}
+	
+	public void updatePlayerWallet(int player, int amount) {
+		
+	}
+	
+	public void updateOwnership(int player, int fieldnum) {
+		
+	}
+	
+	public void updateBuildings(int fieldnum, int houses, int hotel) {
+		
+	}
+	
+	public void writeText (String text) {
+		
+	}
+	
+	public void showChanceCard (String text) {
+		
+	}
+}
