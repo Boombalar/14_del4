9d4e70e14a2a8c81a56c120d0fcfa3e4ec499c0f
KimSand <s@ndberg.dk>
2018-01-05 16:31:07 +0100

    ChanceCard skabelon færdig.

---
+++ 14_del_final/src/ChanceCardCTRL.java
@@ -0,0 +1,18 @@
+
+public class ChanceCardCTRL {
+	ChanceCard[] chancecard = new ChanceCard[30];
+
+	public ChanceCardCTRL () {
+		
+	}
+	
+	private void shuffle() {
+		
+	}
+	
+	public int[] draw() {
+		int[] values = new int[2];
+		
+		return values;
+	}
+}
