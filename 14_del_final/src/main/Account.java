fa86fa688c4a5ed1f7e2f81da4bb5139506a7c30
Mathias <mathias_1337@hotmail.com>
2018-01-06 13:14:37 +0100

    Stavefejl i player

---
+++ 14_del_final/src/main/Account.java
@@ -0,0 +1,31 @@
+package main;
+
+public class Account {
+	private int balance = 30000; //Start værdi for hver spiller.
+	
+	/**
+	 * getBalance som returnere den givne spillers nuværende pengebeholdning.
+	 * @return Spillers nuværende pengebeholdning.
+	 */
+	public int getBalance() {
+		return balance;
+	}
+	
+	/**
+	 * Withdraw(Udtræk) penge fra den givne spillers pengebeholdning.
+	 * @param value den mængde der skal trækkes ud af spillerens account.
+	 */
+	
+	public void withdraw(int value) {
+		this.balance -= value;
+	}
+	
+	/**
+	 * deposit(indsæt) penge til den givne spillers pengebeholdning.
+	 * @param value den mængde der skal lægges til spillerens account.
+	 */
+	
+	public void deposit(int value) {
+		this.balance += value;
+	}
+}
