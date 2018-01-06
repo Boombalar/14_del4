1628ad4cafe58b4a381bf0b96422514b1a4eb5f6
KimSand <s@ndberg.dk>
2018-01-06 11:24:41 +0100

    rettet til med getReturnValue

--- 14_del_final/src/TaxField.java
+++ 14_del_final/src/TaxField.java
@@ -1,13 +1,14 @@
 
 public class TaxField extends Field{
-	int taxAmount;
-
+	
+	private int[] returnvalue = new int[1];
+	
 	public TaxField (String name, int type, int number, int taxAmount) {
 		super(name,type,number);
-		this.taxAmount = taxAmount;
+		this.returnvalue[1] = taxAmount;
 	}
 
-	public int getTaxAmount() {
-		return this.taxAmount;
+	public int[] getReturnValue() {
+		return this.returnvalue;
 	}
 }
