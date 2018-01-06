e5acac3f9f10246ad92d80cfe1dc5805576ae6a9
KimSand <s@ndberg.dk>
2018-01-06 13:38:45 +0100

    ny parameter liste

--- 14_del_final/src/PropertyFields.java
+++ 14_del_final/src/PropertyFields.java
@@ -5,13 +5,17 @@ public class PropertyFields extends OwnerFields {
 	//private int numberOfHouse;
 	//private int priceOfHouse;
 	
-	public PropertyFields (String name, int type, int number, int popertyValue, int owner, int groupNumber, int[] rent) {
+	public PropertyFields (String name, int type, int number, int popertyValue, int owner, int groupNumber, int value0, int value1, int value2, int value3, int value4, int value5, int value6, int value7) {
     	super(name, type, number, popertyValue, owner, groupNumber);
-		
-		
-		this.rent = rent;
-
-		
+		rent[0] = value0;
+		rent[1] = value1;
+		rent[2] = value2;
+		rent[3] = value3;
+		rent[4] = value4;
+		rent[5] = value5;
+		rent[6] = value6;
+		rent[7] = value7;
+				
 	}
 	public int[] getReturnValue() {
 		return this.rent;
---
+++ 14_del_final/src/PropertyFieldsTest.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PropertyFieldsTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testPropertyFields() {
+		PropertyFields propertyfield = new PropertyFields("Hvidovrevej",1,4,1200,0,1,50,250,750,2250,4000,6000,0,1000);
+	new PropertyFields();
+	}
+
+}
