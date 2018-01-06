2f88a24bf51b90fe2179f7027eeb274562f53a69
Mathias <mathias_1337@hotmail.com>
2018-01-06 14:19:22 +0100

    Flyttede rundt på default package til main package

---
+++ 14_del_final/src/main/ViewCTRL.java
@@ -0,0 +1,56 @@
+package main;
+
+public class ViewCTRL {
+
+	
+	
+	public ViewCTRL(Player[] players, Board board) {
+		
+	}
+	
+	public String getDropDownChoice(String buttontext, String[] lines) {
+		String choice = "";
+		
+		return choice;
+	}
+	
+	public int getButtonPressed(String buttontext) {
+		int choice;
+		
+		return choice;
+	}
+	
+	public String getTextField() {
+		String enteredText="";
+		
+		return enteredText;
+	}
+	
+	public void updateDice(int die1, int die2) {
+		
+	}
+	
+	public void updatePlayerPosition(int player, int position) {
+		
+	}
+	
+	public void updatePlayerWallet(int player, int amount) {
+		
+	}
+	
+	public void updateOwnership(int player, int fieldnum) {
+		
+	}
+	
+	public void updateBuildings(int fieldnum, int houses, int hotel) {
+		
+	}
+	
+	public void writeText (String text) {
+		
+	}
+	
+	public void showChanceCard (String text) {
+		
+	}
+}
