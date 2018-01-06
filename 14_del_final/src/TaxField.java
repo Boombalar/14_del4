1d6ea04f1d3edd7e542eff7de49e430dbc1cc756
KimSand <s@ndberg.dk>
2018-01-06 11:38:43 +0100

    ændret til ny navnekonvention

--- 14_del_final/src/TaxField.java
+++ 14_del_final/src/TaxField.java
@@ -1,14 +1,14 @@
 
 public class TaxField extends Field{
 	
-	private int[] returnvalue = new int[1];
+	private int[] returnValue = new int[1];
 	
 	public TaxField (String name, int type, int number, int taxAmount) {
 		super(name,type,number);
-		this.returnvalue[1] = taxAmount;
+		this.returnValue[1] = taxAmount;
 	}
 
 	public int[] getReturnValue() {
-		return this.returnvalue;
+		return this.returnValue;
 	}
 }
