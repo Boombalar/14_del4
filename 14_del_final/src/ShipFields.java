920b3496f67b217511c1218fde0a842abb59cf29
Mettelba <s172840@student.dtu.dk>
2018-01-06 14:18:50 +0100

    ShipFields ændret i parameter value0-value3

--- 14_del_final/src/ShipFields.java
+++ 14_del_final/src/ShipFields.java
@@ -3,9 +3,12 @@ public class ShipFields extends OwnerFields {
 	
 	int[] rent = new int[4]; //array på 6
 	
-	public ShipFields (String name, int type, int number, int propertyValue, int owner, int groupNumber, int[] rent) {
+	public ShipFields (String name, int type, int number, int propertyValue, int owner, int groupNumber, int value0, int value1, int value2, int value3 ) {
 		super(name, type, number, propertyValue, owner, groupNumber);
-	
+		rent[0] = value0;
+		rent[1] = value1;
+		rent[2] = value2;
+		rent[3] = value3;
 		
 	}
 	public int[] returnValue() {
