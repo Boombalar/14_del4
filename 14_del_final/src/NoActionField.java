ae08172a3c773cfb6f81d2cca6cc29cab440d8ac
KimSand <s@ndberg.dk>
2018-01-06 15:13:45 +0100

    Nu med kommentar

--- 14_del_final/src/NoActionField.java
+++ 14_del_final/src/NoActionField.java
@@ -1,6 +1,8 @@
 
 public class NoActionField extends Field{
-
+/** 
+ * Her er der ingen returværdi.
+ */
 	public NoActionField(String name, int type, int number) {
 		super(name, type, number);
 		
