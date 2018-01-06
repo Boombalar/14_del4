0d98be8404fa70b33fe2a8aa0308fa33f7639bee
Niklaes Jacobsen <niklaes@gmail.com>
2018-01-06 15:49:00 +0100

    BoardTest add

---
+++ 14_del_final/src/test/BoardTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BoardTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testBoard() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFieldType() {
+		//fail("Not yet implemented");
+	}
+
+}
