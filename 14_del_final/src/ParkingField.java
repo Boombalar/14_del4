d5bb54c069a0a607bfd55186712b8fda20df3eb4
KimSand <s@ndberg.dk>
2018-01-06 15:46:52 +0100

    Nu med kommentar

--- 14_del_final/src/ParkingField.java
+++ 14_del_final/src/ParkingField.java
@@ -1,4 +1,8 @@
-
+/**
+ * Ingen returværdi
+ * @author kimsa
+ *
+ */
 public class ParkingField extends Field {
 	public ParkingField(String name, int type, int number) {
 		super(name, type, number);
