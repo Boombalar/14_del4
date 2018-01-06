4d916191cefcf76b268a60a4c07ba1b51e5273ba
Mette <Mette@Lenovo-PC>
2018-01-06 10:56:35 +0100

    ChanceCard klasse

--- 14_del_final/src/ChanceCard.java
+++ 14_del_final/src/ChanceCard.java
@@ -1,4 +1,31 @@
 
+
 public class ChanceCard {
 
+	protected int number;
+	protected int type;
+	protected String description;
+	
+	
+	public ChanceCard(int number,int type,String description) {
+		this.number=number;
+		this.type=type;
+		this.description=description;
+		
+	}
+	
+	public int getNumber() { 
+		return this.number;
+	}
+	
+	public int getType() {
+		return this.type;
+	}
+	
+	public String getDescription() {
+		return this.description;
+	}
+	
+	
 }
+ 
