e751d9c669e62130de8e9db82a3b59e4ba4130c1
Unknown <s@ndberg.dk>
2018-01-07 14:51:34 +0100

    Færdig Board mangler helt rigtige data.

--- 14_del_final/src/main/model/Board.java
+++ 14_del_final/src/main/model/Board.java
@@ -4,132 +4,92 @@ public class Board {
 
 	private Field[] field;
 
-
-
-
-
-	public int getFieldType(int type) {
-		Field[type].getType;
-		return type;
-	}
-
-
 	public Board() {
 		//opret felter
 
+		field[0] = new StartField("Start", 5, 0);
 
+		field[1] = new PropertyFields("Rødovrevej",0,1,1200,0,1,50,250,750,2250,4000,6000,0,400);
 
-		field[0] = new StartField("Start", 4, 1);
+		field[2] = new ChanceField("Prøv Lykken",4,2);
 
-		int[] rent = {50,250,750,2250,4000,6000};
-		field[1] = new PropertyFields("Rødovrevej",1,2,rent,0,0);
+		field[3] = new PropertyFields("Hvidovrevej",0,3,1200,0,1,50,250,750,2250,4000,6000,0,400);
 
-		field[2] = new ChanceField("Prøv Lykken",4,3);
+		field[4] = new TaxField("Betal indkomst Skat",3,4,4000);
 
-		int[] rent1 = {50,250,750,2250,4000,6000};
-		field[3] = new PropertyFields("Hvidovrevej",1,4,rent1,0,0);
+		field[5] = new ShipFields("SFL",1,5,4000,0,9,500,1000,2000,4000);
 
-		int[] tax = {4000};
-		field[4] = new TaxField("Betal indkomst Skat",3,5,tax);
+		field[6] = new PropertyFields("Roskildevej",0,6,2000,0,2,100,600,1800,5400,8000,11000,0,500);
 
-		int[] rent2 = {500,1000,2000,4000};
-		field[5] = new ShipFields("SFL",1,6,rent2);
+		field[7] = new ChanceField("Prøv lykken", 4, 7);
 
-		int[] rent3 = {100,600,1800,5400,8000,11000};
-		field[6] = new PropertyFields("Roskildevej",1,7, rent3,0,0);
+		field[8] = new PropertyFields("Valby Langgade",0,8,2000,0,2,100,600,1800,5400,8000,11000,0,500);
 
-		field[7] = new ChanceField("Prøv lykken", 4, 8);
+		field[9] = new PropertyFields("Allégade",0,9,2800,0,2,100,600,1800,5400,8000,11000,0,500);
 
-		int[] rent4 = {100,600,1800,5400,8000,11000};
-		field[8] = new PropertyFields("Valby Langgade",1,9, rent4,0,0);
+		field[10] = new NoActionField("På Besøg",6,10);
 
-		int[] rent5 = {150,800,2000,6000,9000,12000};
-		field[9] = new PropertyFields("Allégade",1,10,rent5,0,0);
+		field[11] = new PropertyFields("Frederiksberg Allè",0,11,3200,0,3,200,1000,3000,9000,12500,15000,0,800);
 
-		field[10] = new NoActionField("På Besøg",4,11);
+		field[12] = new BreweryFields("Tuborg",2,12,3000, 0, 10,100,200);
 
-		int[] rent6 = {200,1000,3000,9000,12500,15000};
-		field[11] = new PropertyFields("Frederiksberg Allè",1,12,rent6,0,0);
+		field[13] = new PropertyFields("Bülowsvej",0,13,3200,0,3,200,1000,3000,9000,12500,15000,0,800);
 
-		int[] rent7 = {100,200};
-		field[12] = new BreweryFields("Tuborg",1,13,rent7);
+		field[14] = new PropertyFields("Gl.Kongevej", 0,14,3800,0,3,250,1250,3750,10000,14000,18000,0,800);
 
-		int[] rent8 = {200,1000,3000,9000,12500,15000};
-		field[13] = new PropertyFields("Bülowsvej",1,14,0,0);
+		field[15] = new ShipFields("Kalundborg/Århus",1,15,4000,0,9,500,1000,2000,4000);
 
-		int[] rent9 = {250,1250,3750,10000,14000,18000};
-		field[14] = new PropertyFields("Gl.Kongevej", 1,15,rent9,0,0);
+		field[16] = new PropertyFields("Bernstorffsvej",0,16,3800,0,4,300,1400,4000,11000,15000,19000,0,1000);
 
-		int[] rent10 = {500,1000,2000,4000};
-		field[15] = new ShipFields("Kalundborg/Århus",1,16,rent10);
+		field[17] = new ChanceField("Prøv Lykken",4,17);
 
-		int[] rent11 = {300,1400,4000,11000,15000,19000};
-		field[16] = new PropertyFields("Bernstorffsvej",1,17,rent11,0,0);
+		field[18] = new PropertyFields("Hellerupvej",0,18,3800,0,4,300,1400,4000,11000,15000,19000,0,1000);
 
-		field[17] = new ChanceField("Prøv Lykken",4,18);
+		field[19] = new PropertyFields("Strandvej",0,19,4200,0,4,350,1600,4400,12000,16000,20000,0,1000);
 
-		int[] rent12 = {300,1400,4000,11000,15000,19000};
-		field[18] = new PropertyFields("Hellerupvej",1,19,rent12,0,0);
+		field[20] = new NoActionField("Parkering", 6,20);
 
-		int[] rent13 = {350,1600,4400,12000,16000,20000};
-		field[19] = new PropertyFields("Strandvej",1,20,rent13,0,0);
+		field[21] = new PropertyFields("Trianglen",0,21,4500,0,5,350,1800,5000,14000,17500,21000,0,1200);
 
-		field[20] = new NoActionField("Parkering", 4,21);
+		field[22] = new ChanceField("Prøv Lykken", 4,22);
 
-		int[] rent14 = {350,1800,5000,14000,17500,21000};
-		field[21] = new PropertyFields("Trianglen",1,22,rent14,0,0);
+		field[23] = new PropertyFields("Østerbrogade",0,23,4500,0,5,350,1800,5000,14000,17500,21000,0,1200);
 
-		field[22] = new ChanceField("Prøv Lykken", 4,23);
+		field[24] = new PropertyFields("Grønningen",0,24,4500,0,5,400,2000,6000,15000,18500,22000,0,1200);
 
-		int[] rent15 = {350,1800,5000,14000,17500,21000};
-		field[23] = new PropertyFields("Østerbrogade",1,24,rent15,0,0);
+		field[25] = new ShipFields("DFDS Seaways",1,25,4000,0,9,500,1000,2000,4000);
 
-		int[] rent16 = {400,2000,6000,15000,18500,22000};
-		field[24] = new PropertyFields("Grønningen",1,25,rent16,0,0);
+		field[26] = new PropertyFields("Bredgade",0,26,4500,0,6,450,2200,6600,16000,19500,23000,0,1500);
 
-		int[] rent17 = {500,1000,2000,4000};
-		field[25] = new ShipFields("DFDS Seaways",1,26,rent17);
-
-		int[] rent18 = {450,2200,6600,16000,19500,23000};
-		field[26] = new PropertyFields("Bredgade",1,27,rent18,0,0);
-
-		int[] rent19 = {450,2200,6600,16000,19500,23000};
-		field[27] = new PropertyFields("Kgs.Nytorv",1,28,rent19,0,0);
-
-		int[] rent20 = {100,200};
-		field[28] = new BreweryFields("Coca-Cola",1,29,rent20);
-
-		int[] rent21 = {500,2400,7200,17000,20500,24000};
-		field[29] = new PropertyFields("Østergade",1,30,rent21,0,0);
-
-		field[30] = new GoToJailField("De Fængsles",2,31,11);
-
-		int[] rent22 = {550,2600,7800,18000,22000,25000};
-		field[31] = new PropertyFields("Amagertorv",1,32,rent22,0,0);
-
-		int[] rent23 = {550,2600,7800,18000,22000,25000};
-		field[32] = new PropertyFields("Vimmelskaftet",1,33,rent23,0,0);
-
-		field[33] = new ChanceField("PrøvLykken",4,34);
-
+		field[27] = new PropertyFields("Kgs.Nytorv",0,27,4500,0,6,450,2200,6600,16000,19500,23000,0,1500);
 		
-		int[] rent24 = {600,3000,9000,20000,24000,28000};
-		field[34] = new PropertyFields("Nygade",1,35,rent24,0,0);
+		field[28] = new BreweryFields("Coca-Cola",2,28,3000, 0, 10,100,200);
 
-		int[] rent25 = {500,1000,2000,4000};
-		field[35] = new ShipFields("Halsskov/Knudshoved",1,36,rent25);
+		field[29] = new PropertyFields("Østergade",0,29,5000,0,6,500,2400,7200,17000,20500,24000,0,1800);
 
-		field[36] = new NoActionField("Prøv Lykken",4,37);
+		field[30] = new GoToJailField("De Fængsles",7,30,11);
 
-		int[] rent26 = {700,3500,10000,22000,26000,30000};
-		field[37] = new PropertyFields("Frederiksberggade",1,38,rent26,0,0);
+		field[31] = new PropertyFields("Amagertorv",0,31,5000,0,7,550,2600,7800,18000,22000,25000,0,2000);
 
-		int[] rent27 = {2000};
-		field[38] = new TaxField("Betal skat",3,39,rent27);
+		field[32] = new PropertyFields("Vimmelskaftet",0,32,5000,0,7,550,2600,7800,18000,22000,25000,0,2000);
 
-		int[] rent28 = {1000,4000,12000,28000,34000,40000};
-		field[39] = new PropertyFields("Rådhuspladsen",1,40,rent28,0,0);
+		field[33] = new ChanceField("PrøvLykken",4,33);
 
+		field[34] = new PropertyFields("Nygade",0,34,5400,0,7,600,3000,9000,20000,24000,28000,0,2000);
+
+		field[35] = new ShipFields("Halsskov/Knudshoved",1,35,4000,0,9,500,1000,2000,4000);
+
+		field[36] = new NoActionField("Prøv Lykken",4,36);
+
+		field[37] = new PropertyFields("Frederiksberggade",0,37,7000,0,8,700,3500,10000,22000,26000,30000,0,3000);
+
+		field[38] = new TaxField("Betal skat",3,38,2000);
+
+		field[39] = new PropertyFields("Rådhuspladsen",0,39,8000,0,8,1000,4000,12000,28000,34000,40000,0,4000);
+	}
+	
+	public Field[] getFields() {
+		return field;
 	}
 }
 
--- 14_del_final/src/main/model/PropertyFields.java
+++ 14_del_final/src/main/model/PropertyFields.java
@@ -8,15 +8,29 @@ public class PropertyFields extends OwnerFields {
 	Er lagt in i det sidste to pladser i arrayet. 
 	*/
 	
+	/**
+	 * 
+	 * @param propertyValue	hvad koster feltet
+	 * @param owner			hvem ejer felete
+	 * @param groupNumber	Gruppenummer eks. hvidovrevej, og rødovrevej er gruppe 1
+	 * @param value0	rent 0 huse
+	 * @param value1	rent 1 huse
+	 * @param value2	rent 2 huse
+	 * @param value3	rent 3 huse
+	 * @param value4	rent 4 huse
+	 * @param value5	rent 5 huse/Hotel
+	 * @param value6   antal huse på felt.
+	 * @param value7   Pris for at bygge hus.
+	 */
 	public PropertyFields (String name, int type, int number, int propertyValue, int owner, int groupNumber, int value0, int value1, int value2, int value3, int value4, int value5, int value6, int value7) {
     	super(name, type, number, propertyValue, owner, groupNumber);
-		rent[0] = value0;
+		rent[0] = value0; 
 		rent[1] = value1;
 		rent[2] = value2;
 		rent[3] = value3;
 		rent[4] = value4;
 		rent[5] = value5;
-		rent[6] = value6;
+		rent[6] = value6; 
 		rent[7] = value7;
 				
 	}
