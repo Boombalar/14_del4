6aa5286bdf5df5dccae3e3d3af739389285bb443
Kim Sandberg <s@ndberg.dk>
2018-01-08 15:27:20 +0100

    ChanceCardController

--- 14_del_final/src/controller/ChanceCardCTRL.java
+++ 14_del_final/src/controller/ChanceCardCTRL.java
@@ -1,19 +1,56 @@
 package controller;
 
-public class ChanceCardCTRL {
-	ChanceCard[] chanceCard = new ChanceCard[30];
+import model.ChanceCardDeck;
+import model.ChanceCard;
+import java.util.Random;
 
+public class ChanceCardCTRL {
+	ChanceCard[] chanceCard;
+	int cardPointer = 0;
+	int cardnumber = 0;
+	
+	
 	public ChanceCardCTRL () {
-		
+		ChanceCardDeck deck = new ChanceCardDeck();
+		chanceCard = deck.getChanceCards();
 	}
 	
 	private void shuffle() {
-		
+	        for (int i = 0;i < 60000; i++) {
+	            Random random = new Random();
+	            int plads = random.nextInt(chanceCard.length);
+	            int tal = chanceCard[plads].getNumber();
+	            int plads2 = random.nextInt(chanceCard.length);
+	            chanceCard[plads]= chanceCard[plads2];
+	            chanceCard[plads2].setNumber(tal);
+	        }
+	        for (int i = 0; i < chanceCard.length; i++) {
+	            System.out.println(chanceCard[i]);                       
+	        }
+	        System.out.println(chanceCard[2]);	
 	}
 	
-	public int[] draw() {
-		int[] returnValues = new int[2];
+	public void draw() {
+		if (cardPointer > 31) {
+			shuffle();
+			cardPointer = 0;
+		}
 		
-		return returnValues;
+		cardnumber = chanceCard[cardPointer].getType();
+		cardPointer = cardPointer +1;
 	}
+	
+	public int[] getReturnValue() {
+		return chanceCard[cardnumber].returnValue();
+	}
+	
+	public String getDescription() {
+		return chanceCard[cardnumber].getDescription();
+	}
+
+	public int getType() {
+		return chanceCard[cardnumber].getType();
+	}
+	
+	
 }
