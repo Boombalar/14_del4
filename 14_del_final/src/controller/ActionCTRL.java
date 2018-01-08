a42a6192cb84b56c57fa53f87fc2889fb05254ea
Unknown <s172840@student.dtu.dk>
2018-01-08 15:49:16 +0100

    mergeconflict dealed with.

--- 14_del_final/src/controller/ActionCTRL.java
+++ 14_del_final/src/controller/ActionCTRL.java
@@ -1,23 +1,58 @@
 package controller;
 
+import model.Board;
+import model.Player;
+import view.ViewCTRL;
+import model.DieCup;
+
+
 public class ActionCTRL {
 	model.Player[] players;
 	model.Board board;
 	view.ViewCTRL view;
 	ChanceCardCTRL chanceCard;
 	model.DieCup diecup;
+	private int playernum;
 	
 	public ActionCTRL() {
-		view.ViewCTRL view = new view.ViewCTRL(this.players, this.board);
 	}
 	
 	private void initialiseGame() {
+		Player[] players = new Players();
+		Board board = new Board();
+		ViewCTRL viewCRTL = new ViewCTRL();
+		ChanceCardCTRL chanceCardCRTL = new ChanceCardCRTL();
+		DieCup dieCup = new DieCup();
 		
+		int Player[]
+		
+	
+	private void gameSequences();
+	 	if (players[currentPlayer].checkBroke)
+	 		i++
+	//hele gameSequences while loop i et while loop i et forloop. 
+	//metodee der har logikken bag alle spillers tur 
 	}
+	private void tansfer(int currentPlayer, int	receivePlayer, boolean direction, int amount) {
+		if (direction == true);
+		players[currentPlayer].removemoney(amount);
+		players[receivePlayer].recievemoney(amount);
+		
+		//recievemoney og removemoney. 
+		}else { (direction == false);
+		players[currentPlayer].recieveMoney(amount);
+		players[receivePlayer].removeMoney.(amount);	
+}
+	
 	
 	private void switchRules() {
 		
 	}
+	
+	
+	private void startGame();
+	String[] playerAmount = {6, 5, 4, 3, 2};
+	playernum = 
 	
 	//REGEL METODER HERUNDER.
 }
--- 14_del_final/src/controller/StartGame.java
+++ 14_del_final/src/controller/StartGame.java
@@ -1,11 +1,13 @@
 package controller;
 
+import controller.ActionCTRL;
+
 public class StartGame {
 	private ActionCTRL actionCtrl;
 	private int playernum;
 
 	public static void main(String[] args) {
-		controller.ActionCTRL ac = new controller.ActionCTRL();
+		ActionCTRL actionCRTL = new controller.ActionCTRL();
 	}
 	
 	public StartGame() {
